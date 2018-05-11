d5292a48cdbdfd71178b2b9487e016f4d8015775
Vignesh, Arun <arun.vignesh@pearson.com>
2018-05-11 20:41:50 +0530

    Changing UI pattern

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var app = require("express")(),
+var express = require("express"),
+    app = express(),
     path = require("path");
 
 app.set("views", path.join(__dirname,"./views"));
 app.set("view engine", "html");
 app.engine("html", require("ejs").renderFile);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/",(req, res) => {
     res.render("index");
+});
+
+app.get("/enquiry",(req, res) => {
+    res.render("enquiry");
 });
 
 app.get("*",(req, res) => {
