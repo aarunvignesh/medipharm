7947c33f63c9a19c15cd17488ffef67c559fe680
Vignesh, Arun <arun.vignesh@pearson.com>
2018-07-21 15:04:46 +0530

    Adding search

---
+++ client/search/component/search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Component({
+    selector:"search-component",
+    styleUrls:["./../style/search.css"],
+    templateUrl: "./../template/search.html"
+})
+export class searchComponent{
+ 
+}
