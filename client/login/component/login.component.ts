d7ad4ad88f16e6920adccd97a388503ca9f55e3e
Vignesh, Arun <arun.vignesh@pearson.com>
2018-07-21 13:45:30 +0530

    Adding components

---
+++ client/login/component/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Component({
+    selector:"core-component",
+    styleUrls:["./../style/login.css"],
+    templateUrl: "./../template/login.html"
+})
+export class loginComponent{
+ 
+}
