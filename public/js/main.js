ec21b537a263209512c70cdef208b6bcf908ec82
Vignesh, Arun <arun.vignesh@pearson.com>
2018-09-13 15:36:58 +0530

    Changes done

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,14 @@
 
 (function ($) {
     "use strict";
-
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var company = $('.validate-input input[name="companyName"]');
-    var contact = $('.validate-input input[name="contactNumber"]');
-
-
     $('.enquiry-form-btn').on('click',function(e){
         e.preventDefault();
         var check = true;
+
+        var name = $('#modalName');
+        var email = $('#modalEmail');
+        var company = $('#modalCompany');
+        var contact = $('#modalContact');
 
         if($(name).val().trim() == ''){
             showValidate(name);
@@ -36,7 +34,7 @@
             var formData = new FormData(this);    
             
             $('.enquiry-form-btn').hide();
-            var payload = new FormData($(".enquiry-form.validate-form")[0]);
+            var payload = new FormData($(".enquiry-form.validate-form.modal-form")[0]);
 			// $(".enquiry-form.validate-form").serializeArray().forEach(function(val){
 			// 	payload[val.name] = val.value;
             // });
@@ -49,11 +47,77 @@
                 contentType: false,
                 processData: false
 			}).done(function(){
-				$(".notify-msg").show();
-				setTimeout(function(){
-					$(".notify-msg").hide();
-					window.location.href = "/";
-				},5000);
+                $('.enquiry-form-btn').show();
+                name.val("");
+                email.val("");
+                company.val("");
+                contact.val("");
+                $("#modalMessage").val("");
+                closeEnquiry();
+			});
+        }
+        return check;
+    });
+
+
+    $('.validate-form .input1').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    $('.index-form-btn').on('click',function(e){
+        e.preventDefault();
+        var check = true;
+
+        var name = $('#indexName');
+        var email = $('#indexEmail');
+        var company = $('#indexCompany');
+        var contact = $('#indexContact');
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(company).val().trim() == ''){
+            showValidate(company);
+            check=false;
+        }
+
+        if($(contact).val().trim() == ''){
+            showValidate(contact);
+            check=false;
+        }
+        console.log(check);
+        if(check){
+            var formData = new FormData(this);    
+            
+            $('.index-form-btn').hide();
+            var payload = new FormData($(".index-form")[0]);
+			// $(".enquiry-form.validate-form").serializeArray().forEach(function(val){
+			// 	payload[val.name] = val.value;
+            // });
+
+			$.ajax({
+				url:"./enquiry",
+				method: "post",
+                data: payload,
+                cache: false,
+                contentType: false,
+                processData: false
+			}).done(function(){
+                $('.index-form-btn').show();
+                name.val("");
+                email.val("");
+                company.val("");
+                contact.val("");
+                $("#indexMessage").val("");
 			});
         }
         return check;
